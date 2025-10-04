ef42810bbdb1e1cbe576d51ce31e8465a1068704
Rishikesh Singh <er.rishikeshsingh0@gmail.com>
2025-10-04 19:54:52 +0530

    payment gateway integrated

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,29 +1,88 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import "./PlaceOrder.css";
 import { StoreContext } from '../../context/StoreContext';
+import axios from 'axios';
 
 const PlaceOrder = () => {
-    const { getTotalCartAmount } = useContext(StoreContext);
+    const { getTotalCartAmount, token, food_list, server_url, cartItems } = useContext(StoreContext);
+
+    const [data, setData] = useState({
+        firstName: "",
+        lastName: "",
+        email: "",
+        street: "",
+        city: "",
+        state: "",
+        zipCode: "",
+        country: "",
+        phone: ""
+    });
+
+    const onChangeHandler = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setData((data)=>({...data, [name]:value}));
+    }
+
+    const placeOrder = async(event) => {
+    event.preventDefault();
+    let orderItems = [];
+    food_list.map((item) => {
+        if (cartItems[item._id] > 0) {
+            let itemInfo = item;
+            itemInfo["quantity"] = cartItems[item._id];
+            orderItems.push(itemInfo);
+        }
+    });
+    let orderData = {
+        address: data,
+        items: orderItems,
+        amount: getTotalCartAmount() + 15
+    };
+
+    try {
+        let response = await axios.post(
+            server_url + "/api/order/place",
+            orderData,
+            {
+                headers: {
+                    token: token   // ✅ FIX HERE
+                }
+            }
+        );
+
+        if (response.data.success) {
+            const { session_url } = response.data;
+            window.location.replace(session_url);
+        } else {
+            alert("Error");
+        }
+    } catch (error) {
+        console.error("Order Error:", error);
+        alert("Something went wrong!");
+    }
+};
+
 
     return (
-        <form className='place-order' onClick={(e)=>e.preventDefault()}>
+        <form onSubmit={placeOrder} className='place-order'>
             <div className="place-order-left">
                 <p className="title">Delivery Information</p>
                 <div className="multi-fields">
-                    <input type="text" placeholder='First name' required />
-                    <input type="text" placeholder='Last name' required />
+                    <input name='firstName' onChange={onChangeHandler} value={data.firstName} type="text" placeholder='First name' required />
+                    <input name='lastName' onChange={onChangeHandler} value={data.lastName} type="text" placeholder='Last name' required />
                 </div>
-                <input type="email" placeholder='Email address' required />
-                <input type="text" placeholder='Street' required />
+                <input name='email' onChange={onChangeHandler} value={data.email} type="email" placeholder='Email address' required />
+                <input name='street' onChange={onChangeHandler} value={data.street} type="text" placeholder='Street' required />
                 <div className="multi-fields">
-                    <input type="text" placeholder='City' required />
-                    <input type="text" placeholder='State' required />
+                    <input name='city' onChange={onChangeHandler} value={data.city} type="text" placeholder='City' required />
+                    <input name='state' onChange={onChangeHandler} value={data.state} type="text" placeholder='State' required />
                 </div>
                 <div className="multi-fields">
-                    <input type="number" placeholder='Zip code' required />
-                    <input type="text" placeholder='Country' required />
+                    <input name='zipCode' onChange={onChangeHandler} value={data.zipCode} type="number" placeholder='Zip code' required />
+                    <input name='country' onChange={onChangeHandler} value={data.country} type="text" placeholder='Country' required />
                 </div>
-                <input type="number" placeholder='Phone' required />
+                <input name='phone' onChangeCapture={onChangeHandler} value={data.phone} type="number" placeholder='Phone' required />
             </div>
             <div className="place-order-right">
                 <div className="cart-total">
@@ -44,7 +103,7 @@ const PlaceOrder = () => {
                             <b>{"\u20B9"} {getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 15}</b>
                         </div>
                     </div>
-                    <button>PROCEED TO PAY</button>
+                    <button type='submit'>PROCEED TO PAY</button>
                 </div>
             </div>
         </form>
