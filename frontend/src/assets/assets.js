4afd0501309d8849a3e2500fe1b0b01f5e85b104
Rishikesh Singh <er.rishikeshsingh0@gmail.com>
2025-10-04 11:49:53 +0530

    auth connection integrated

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -59,6 +59,7 @@ import profile_icon from './profile_icon.png'
 import bag_icon from './bag_icon.png'
 import logout_icon from './logout_icon.png'
 import parcel_icon from './parcel_icon.png'
+import profile_image from './profile_image.png'
 
 export const assets = {
     logo,
@@ -79,7 +80,8 @@ export const assets = {
     profile_icon,
     logout_icon,
     bag_icon,
-    parcel_icon
+    parcel_icon,
+    profile_image
 }
 
 export const menu_list = [
